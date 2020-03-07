aac21959a1631bb615484eec8f3e66f8b527c39d
Milad Khajavi <khajavi@gmail.com>
2020-03-07 15:00:03 +0330

    add parallel sample.

---
+++ src/main/scala/ParallelExample.scala
@@ -0,0 +1,56 @@
+import cats.Applicative
+
+import scala.concurrent.Future
+
+object ParallelExample extends App {
+  import cats.instances.future._
+  import cats.syntax.all._
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  val a = Applicative[Future].pure(1)
+  val b = Applicative[Future].pure(2)
+
+  println((a, b).mapN(_ + _))
+  import cats.~>
+
+  val first: List ~> Option = λ[List ~> Option](_.headOption)
+  // import cats.implicits._
+
+  import cats.data._
+  // import cats.data._
+
+  case class Name(value: String)
+  // defined class Name
+
+  case class Age(value: Int)
+  // defined class Age
+
+  case class Person(name: Name, age: Age)
+  // defined class Person
+
+  def parse(s: String): Either[NonEmptyList[String], Int] = {
+    if (s.matches("-?[0-9]+")) Right(s.toInt)
+    else Left(NonEmptyList.one(s"$s is not a valid integer."))
+  }
+  // parse: (s: String)Either[cats.data.NonEmptyList[String],Int]
+
+  def validateAge(a: Int): Either[NonEmptyList[String], Age] = {
+    if (a > 18) Right(Age(a))
+    else Left(NonEmptyList.one(s"$a is not old enough"))
+  }
+  // validateAge: (a: Int)Either[cats.data.NonEmptyList[String],Age]
+
+  def validateName(n: String): Either[NonEmptyList[String], Name] = {
+    if (n.length >= 8) Right(Name(n))
+    else Left(NonEmptyList.one(s"$n Does not have enough characters"))
+  }
+
+//  def parsePerson(ageString: String, nameString: String) =
+//    for {
+//      age <- parse(ageString)
+//      person <- (validateName(nameString), validateAge(age)).mapN(Person)
+//    } yield person
+
+
+}
