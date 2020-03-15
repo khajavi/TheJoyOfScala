5427f081fa9a6e2d064f07fc57b0700385292395
Milad Khajavi <khajavi@gmail.com>
2020-03-15 12:06:15 +0330

    add kind projector example.

--- kind-projector/src/main/scala/KindProjectorSample.scala
+++ kind-projector/src/main/scala/KindProjectorSample.scala
@@ -1,9 +1,10 @@
+import scala.language.higherKinds
 
 object KindProjectorSample extends App {
   type IntOrA[A] = Either[Int, A]
-  val foo: IntOrA[String] = Right("Milad")
-  val bar: IntOrA[String] = Left(4)
-  println(foo, bar)
+  val foo1: IntOrA[String] = Right("Milad")
+  val bar1: IntOrA[String] = Left(4)
+  println(foo1, bar1)
 
 
   trait Functor[F[_]]
@@ -19,5 +20,29 @@ object KindProjectorSample extends App {
   type F5 = Functor[({type T[A] = Map[Int , A]})#T]
 
   type F6 = Functor[Map[Int, *]]
+
+//  def foo[A[_, _], B](functor: Functor[A[B, ?]]) = ???
+
+  // 1. Type projection
+  def foo[A[_, _], B](functor: Functor[({type AB[C] = A[B, C]})#AB]) = ???
+
+  //2. Kind projector
+  def foo2[A[_, _], B](functor: Functor[A[B, *]]) = ???
+
+  //3. declare surrounding class
+  def foo3[A[_, _], B] = new Foo[A, B]
+
+  class Foo[A[_, _], B] {
+    type AB[C] = A[B, C]
+    def apply(functor: Functor[AB]): Foo[A, B] = new Foo[A, B]
+  }
+
+  //4. curried type constructors
+//  type AB[A, B][C] = A[B, C]   not valid syntax yet
+
+//  def foo[A[_, _], B](functor: Functor[AB[A, B]])
+
+  type a = Functor[Lambda[a => List[Seq[a]]]]
+
 }
 
