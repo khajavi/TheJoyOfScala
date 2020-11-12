5a391ca5deeeeae044b57c254ce62d34694c960b
Milad <khajavi@gmail.com>
2020-11-12 23:07:06 +0330

    update the timer example.

--- src/main/scala/cats/TimerExample.scala
+++ src/main/scala/cats/TimerExample.scala
@@ -4,11 +4,18 @@ import cats.effect.{ExitCode, IOApp, Sync, Timer}
 
 import scala.concurrent.duration.DurationInt
 
-object TimerExample extends IOApp {
+object TimerExample1 extends IOApp {
   override def run(args: List[String]): effect.IO[ExitCode] =
     for {
       _ <- Sync[effect.IO].delay(println("hello"))
       _ <- Timer[effect.IO].sleep(2.seconds)
       _ <- Sync[effect.IO].delay(println("World!"))
     } yield ExitCode.Success
+}
+
+object TimerExample2 extends IOApp {
+  override def run(args: List[String]): effect.IO[ExitCode] =
+    Sync[effect.IO].delay(println("hello")) *>
+      Timer[effect.IO].sleep(2.seconds) *>
+      Sync[effect.IO].delay(println("World!")).as(ExitCode.Success)
 }
